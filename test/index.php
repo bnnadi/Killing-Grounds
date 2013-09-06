ee054789bf3b2eb86c861b47d9c448cf0968d7eb
Jean Amarante <jeanamarantec@gmail.com>
2013-09-06 17:08:51 -0400

    Fix directory problem

---
+++ test/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+
+	<title>Dreamengine</title>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="http://code.createjs.com/createjs-2013.02.12.min.js"></script>
+	<script type="text/javascript" src="dreamengine/core/jquery.hotkeys.js"></script>
+	<script type="text/javascript" src="dreamengine/dreamengine.js"></script>
+	
+	<script type="text/javascript" src="server/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="dreamengine/dreamengine.css">
+
+	<style type="text/css">
+		body {
+			margin: 0;
+			background: #111;
+		}
+		#wrapper {
+			width: 1280px;
+			background: #181818;
+			margin: 30px auto;
+			border: 1px solid #444;
+		}
+	</style>
+
+</head>
+<body>
+	<div id="rpg"></div>
+	<script type="text/javascript" src="game.js"></script>
+</body>
+</html>
